be7050d8424a6233367cca84903b892711754380
Sophia Vogan <sophiavogan@gmail.com>
2020-05-06 13:02:25 -0400

    Create Interaction.cs

---
+++ ReturnHome/Assets/Scripts/Interaction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Interaction : MonoBehaviour
+{
+    private Collider range;
+    protected bool inRange;
+    void Start()
+    {
+        range = GetComponent<Collider>();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player" && Input.GetButtonDown("Interact"))
+        {
+            myInteraction();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRange = false;
+        }
+    }
+    void myInteraction()
+    {
+        print("The object has been interacted with");
+    }
+}
