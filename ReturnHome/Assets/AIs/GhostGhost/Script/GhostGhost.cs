2242f0a6b5a2662965633ce0d00d1c4742d40e0b
llShadowll <aidanlavoie29@gmail.com>
2020-06-03 07:58:19 -0400

    Work on GhostGhost

---
+++ ReturnHome/Assets/AIs/GhostGhost/Script/GhostGhost.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostGhost : GeneralGhost
+{
+    [SerializeField] private GameObject Ghost;
+
+    public override void Attack()
+    {
+        if (attacking)
+        {
+            Instantiate(Ghost, this.transform);
+            base.Attack();
+        }
+    }
+}
