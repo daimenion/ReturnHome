6e807d974474bcad10638c5365630df174755377
llShadowll <aidanlavoie29@gmail.com>
2020-07-01 08:42:41 -0400

    Added No Gravity script

---
+++ ReturnHome/Assets/Scripts/Events/NoGravityEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoGravityEvent : EventController
+{
+    protected override void EventStay(Collider other)
+    {
+        if(other.gameObject.GetComponent<Rigidbody>())
+        {
+            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+        }
+    }
+}
