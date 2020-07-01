47ea664a8cd3fe335aef941f96a075cbcd953989
llShadowll <aidanlavoie29@gmail.com>
2020-07-01 08:50:48 -0400

    Added a simple no gravity event
    The no gravity event checks for objects inside the collider and whether or not they have a rigidbody. If the objects have a rigidbody, upon entering and leaving the effective range, the usegravity boolean is marked as false/true respectively.

--- ReturnHome/Assets/Scripts/Events/NoGravityEvent.cs
+++ ReturnHome/Assets/Scripts/Events/NoGravityEvent.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class NoGravityEvent : EventController
 {
+    //TO DO
+    //Make event only begin when player enters and exits area
+    //Make it affect all objects in the room
     protected override void EventStay(Collider other)
     {
+        
         if(other.gameObject.GetComponent<Rigidbody>())
         {
             other.gameObject.GetComponent<Rigidbody>().useGravity = false;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().useGravity = true;
     }
 }
