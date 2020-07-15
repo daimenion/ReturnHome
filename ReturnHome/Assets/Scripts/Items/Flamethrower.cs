aaaabf8f4d826eca52dfb1e21d5d9d5360ead619
llShadowll <aidanlavoie29@gmail.com>
2020-07-15 12:13:39 -0400

    Added Hairspray and flamethrower prefabs
    Added hairspray and flamethrower prefabs. They have hitboxes, but don't do damage yet.

---
+++ ReturnHome/Assets/Scripts/Items/Flamethrower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flamethrower : Weapon
+{
+    //public boolean to check if it's being used, can change to private later
+    public bool IsUsed;
+    // Update is called once per frame
+    void Update()
+    {
+        interaction();
+    }
+
+    public Flamethrower()
+    {
+        aresol = false;
+        usesLeft = 30.0f;
+        myName = "Flamethrower";
+        description = "Light em up!";
+        type = "Weapon";
+        damage = 15.0f;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //If inside the hitbox, item is used, and other object is an enemy
+        if (WeaponHitBox.bounds.Intersects(other.bounds) && IsUsed && other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            //Do damage to other object
+
+        }
+    }
+}
--- ReturnHome/Assets/Scripts/Items/Hairspray.cs
+++ ReturnHome/Assets/Scripts/Items/Hairspray.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Hairspray : Weapon
 {
+    //public boolean to check if it's being used, can change to private later
+    public bool IsUsed;
     void Update() {
         interaction();
-    
+        
     }
     public Hairspray()
     {
@@ -17,4 +19,22 @@ public class Hairspray : Weapon
         type = "Hair Product";
         damage = 0.5f;
     }
+
+    public override void OnUse()
+    {
+        IsUsed = !IsUsed;
+        base.OnUse();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        //If inside the hitbox, item is used, and other object is an enemy
+        if(WeaponHitBox.bounds.Intersects(other.bounds) && IsUsed && other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            //Do damage to other object
+
+        }
+    }
+
+
 }
