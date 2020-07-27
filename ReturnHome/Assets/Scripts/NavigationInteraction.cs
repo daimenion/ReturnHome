52456d2a97664d9d4c3802bcbaa0a356d34985eb
Sophia Vogan <sophiavogan@gmail.com>
2020-07-27 01:12:10 -0400

    New minigame variant, framework for the endgame, and ghost art

---
+++ ReturnHome/Assets/Scripts/NavigationInteraction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationInteraction : Interaction
+{
+    private CustomGameManager gameManager;
+    // Start is called before the first frame update
+    override protected void Start()
+    {
+        base.Start();
+        gameManager = FindObjectOfType<CustomGameManager>();
+    }
+    override protected void myInteraction()
+    {
+        float health = gameManager.GetShipHealth();
+        if (health >= gameManager.MaxShipHealth*0.80)
+        {
+            print("Congrats, you returned home.");
+        }
+    }
+}
