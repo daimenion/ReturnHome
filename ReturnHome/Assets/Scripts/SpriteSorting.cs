fca600751c9952c027365a888fb61cf06bd63625
Sophia Vogan <sophiavogan@gmail.com>
2020-08-01 14:17:45 -0400

    SpriteSorting

---
+++ ReturnHome/Assets/Scripts/SpriteSorting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteSorting : MonoBehaviour
+{
+    private Transform player;
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>().transform;
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        spriteRenderer.sortingLayerName = "Background";
+    }
+    void Update()
+    {
+        if (player.position.x > transform.position.x || player.position.z > transform.position.z) spriteRenderer.sortingLayerName = "Foreground";
+        else spriteRenderer.sortingLayerName = "Background";
+    }
+}
