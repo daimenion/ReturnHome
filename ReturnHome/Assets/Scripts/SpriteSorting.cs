e7004e8c1b52396e5dee49578a4f2b954ce9681a
Sophia Vogan <sophiavogan@gmail.com>
2020-08-11 21:19:56 -0400

    Changes to Sprite Sorting

--- ReturnHome/Assets/Scripts/SpriteSorting.cs
+++ ReturnHome/Assets/Scripts/SpriteSorting.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class SpriteSorting : MonoBehaviour
 {
     private Transform player;
+    public Vector3 sortpoint = Vector3.zero;
     SpriteRenderer spriteRenderer;
     void Start()
     {
         player = FindObjectOfType<PlayerController>().transform;
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         spriteRenderer.sortingLayerName = "Background";
+        sortpoint = sortpoint + transform.position;
     }
     void Update()
     {
-        if (player.position.x > transform.position.x || player.position.z > transform.position.z) spriteRenderer.sortingLayerName = "Foreground";
+        if (player.position.x > sortpoint.x|| player.position.z > sortpoint.z) spriteRenderer.sortingLayerName = "Foreground";
         else spriteRenderer.sortingLayerName = "Background";
     }
 }
